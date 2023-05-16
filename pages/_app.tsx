1b6af894a1d4dbfa861e68b09ce7cfd589dbd630
JonasRocha189 <jonas.rocha189@gmail.com>
2023-05-16 14:55:46 -0300

    uploading project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
+import { ThemeProvider } from '@mui/material'
+import Cabecalho from '../src/components/Cabecalho/Cabecalho'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import tema from '../src/themes/themes'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={tema}>
+      <Cabecalho />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
